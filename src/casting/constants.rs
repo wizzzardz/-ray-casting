0fb6c4ae27d140c4aa77b5182e882980c6b6efba
Gregory Edison <gregory.edison1993@gmail.com>
2022-12-24 18:24:02 +0100

    feat: player plane vector

--- src/casting/constants.rs
+++ src/casting/constants.rs
@@ -1,1 +1,3 @@
 pub const DISPLACEMENT_STEP: f64 = 10.0;
+pub const FIELD_SPACING: f64 = 100.0;
+pub const SCREEN_WIDTH: f64 = 1080.0;
--- src/casting/player.rs
+++ src/casting/player.rs
@@ -1,21 +1,22 @@
 use super::constants::DISPLACEMENT_STEP;
 use super::types::Vector;
 
-struct Player {
+pub struct Player {
     pub position: Vector,
     pub direction: Vector,
+    pub plane: Vector,
 }
 
 impl Player {
     pub fn new(x: f64, y: f64) -> Self {
         Player {
             position: Vector { x: x, y: y },
-            direction: Vector { x: 0.0, y: 0.0 },
+            direction: Vector { x: 1.0, y: 0.0 },
+            plane: Vector { x: 0.0, y: 0.66 },
         }
     }
 
     pub fn displace(&mut self, direction: &mut Vector) {
-        direction.normalize();
         self.position.x += direction.x * DISPLACEMENT_STEP;
         self.position.y += direction.y * DISPLACEMENT_STEP;
     }
