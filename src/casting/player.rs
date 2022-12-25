ca1040a1b0c31631a0e62be806173528a4030f16
Gregory Edison <gregory.edison1993@gmail.com>
2022-12-25 15:50:27 +0100

    feat: player update

--- src/casting/player.rs
+++ src/casting/player.rs
@@ -1,6 +1,7 @@
-use super::constants::DISPLACEMENT_STEP;
+use super::constants::{DISPLACEMENT_STEP, ROTATE_STEP};
 use super::types::Vector;
 
+#[derive(PartialEq, Debug)]
 pub struct Player {
     pub position: Vector,
     pub direction: Vector,
@@ -10,14 +11,49 @@ pub struct Player {
 impl Player {
     pub fn new(x: f64, y: f64) -> Self {
         Player {
-            position: Vector { x: x, y: y },
+            position: Vector { x, y },
             direction: Vector { x: 1.0, y: 0.0 },
             plane: Vector { x: 0.0, y: 0.66 },
         }
     }
 
-    pub fn displace(&mut self, direction: &mut Vector) {
+    pub fn walk(&mut self, mut direction: Vector) {
+        direction.normalize();
         self.position.x += direction.x * DISPLACEMENT_STEP;
         self.position.y += direction.y * DISPLACEMENT_STEP;
     }
+
+    pub fn turn(&mut self, side: f64) {
+        self.direction.deg_rotate(ROTATE_STEP * side.signum());
+        self.plane.deg_rotate(ROTATE_STEP * side.signum());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let player = Player::new(3.0, 6.0);
+        assert_eq!(
+            Player {
+                position: Vector { x: 3.0, y: 6.0 },
+                plane: Vector { x: 0.0, y: 0.66 },
+                direction: Vector { x: 1.0, y: 0.0 }
+            },
+            player
+        );
+    }
+
+    #[test]
+    fn test_walk() {
+        let mut player = Player::new(3.0, 6.0);
+        let direction = Vector { x: 1.0, y: 2.0 };
+        player.walk(direction);
+        assert!((7.472135 - player.position.x).abs() < 10e6);
+        assert!((14.944271 - player.position.y).abs() < 10e6);
+    }
+
+    //TODO test turn
 }
