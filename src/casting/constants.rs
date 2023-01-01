6d91d83073b84b45d0f11eb88dcb1d2e2ac0f34c
Gregory Edison <gregory.edison1993@gmail.com>
2023-01-01 21:47:15 +0100

    test: ray deltas

--- src/casting/constants.rs
+++ src/casting/constants.rs
@@ -1,5 +1,5 @@
 pub const DISPLACEMENT_STEP: f64 = 1.0;
 pub const ROTATE_STEP: f64 = 1.0;
-pub const FIELD_SPACING: f64 = 100.0;
+pub const FIELD_SPACING: f64 = 1.0;
 pub const SCREEN_WIDTH: f64 = 1080.0;
 pub const INFINITY: f64 = 1_000_000_000_000.0;
--- src/casting/ray.rs
+++ src/casting/ray.rs
@@ -27,13 +27,14 @@ impl<'a> Ray<'a> {
         self.direction = ray;
     }
 
-    pub fn get_initial_deltas(&self, player: Player) -> Vector {
+    pub fn get_initial_deltas(&self) -> Vector {
         let map_square = Vector {
-            x: player.position.x.floor() + 0.5,
-            y: player.position.y.floor() + 0.5,
+            x: self.player.position.x.floor() + 0.5,
+            y: self.player.position.y.floor() + 0.5,
         };
-        let lx = FIELD_SPACING / 2.0 - (player.position.x - map_square.x);
-        let ly = FIELD_SPACING / 2.0 - (player.position.y - map_square.y);
+        let lx = FIELD_SPACING / 2.0 - (self.player.position.x - map_square.x);
+        let ly = FIELD_SPACING / 2.0 - (self.player.position.y - map_square.y);
+        dbg!(lx);
         let mut v = Vector {
             x: lx / self.direction.x,
             y: ly / self.direction.y,
@@ -60,6 +61,42 @@ mod tests {
     fn test_new_ray() {
         let player = Player::new(1.0, 2.0);
         let ray = Ray::new(&player);
+        assert!(ray.direction.x - 0.834609 < 1e-6);
+        assert!(ray.direction.y + 0.550842 < 1e-6);
+    }
+
+    #[test]
+    fn test_update_ray_direction() {
+        let player = Player::new(1.0, 2.0);
+        let mut ray = Ray::new(&player);
+        ray.update_direction(1.0);
+        assert!(ray.direction.x - 0.834609 < 1e-6);
+        assert!(ray.direction.y - 0.550842 < 1e-6);
+        ray.update_direction(0.0);
+        assert!(ray.direction.x - 1.0 < 1e-6);
+        assert!(ray.direction.y < 1e-6);
+    }
+
+    #[test]
+    fn test_get_initial_deltas() {
+        let mut player = Player::new(1.4, 2.3);
+        player.turn(45.0);
+        let mut ray = Ray::new(&player);
+        ray.update_direction(0.0);
+        let deltas = ray.get_initial_deltas();
+        assert!(0.6 / (45.0 / 180.0 * std::f64::consts::PI).cos() - deltas.x < 1e-6);
+        assert!(0.7 / (45.0 / 180.0 * std::f64::consts::PI).sin() - deltas.y < 1e-6);
+    }
+
+    #[test]
+    fn test_get_deltas() {
+        let mut player = Player::new(1.4, 2.3);
+        player.turn(45.0);
+        let mut ray = Ray::new(&player);
+        ray.update_direction(0.0);
+        let deltas = ray.get_deltas();
+        assert!(1.0 / (45.0 / 180.0 * std::f64::consts::PI).cos() - deltas.x < 1e-6);
+        assert!(1.0 / (45.0 / 180.0 * std::f64::consts::PI).sin() - deltas.y < 1e-6);
     }
 }
 
